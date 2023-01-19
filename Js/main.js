36a1ae05d4cee8579da2ca24f508d649d8b19f8b
Miguel Angel Arias <miguel.a.frontend@gmail.com>
2023-01-18 21:18:08 -0500

    correccion clase menu

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,6 @@
 const burgerMobileButton = document.querySelector('.header-container__button');
-const menuMobile = document.querySelector('.header-container__menu');
-const closeMobileButton = document.querySelector('.close-button');
+const menuMobile = document.querySelector('.menu-container');
+const closeMobileButton = document.querySelector('.menu-container__close-button');
 
 burgerMobileButton.addEventListener('click', toggleMobileMenu);
 closeMobileButton.addEventListener('click', toggleMobileMenu);
